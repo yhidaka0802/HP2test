23e2d40ee790b4200a89aea912d17023f2355d11
yu.hidaka <light3223b@gmail.com>
2022-07-05 19:31:58 +0900

    first-commit

---
+++ js/common.js
@@ -0,0 +1,23 @@
+window.onunload = function() {};
+
+function sizecheck() {
+  w = window.innerWidth ? window.innerWidth : $(window).width();
+  h = window.innerHeight ? window.innerHeight : $(window).height();
+}
+
+sizecheck();
+
+$(function () {
+if (w > 2000) {
+} else { 
+  $("header .drawer").click(function(){
+  $('body').toggleClass('nav-open');
+  $('header .navbar').fadeToggle(200);
+    });
+
+  $("header li a").click(function(){
+  $('body').toggleClass('nav-open');
+  $('header .navbar').fadeToggle(200);
+    });
+  }
+}); 
